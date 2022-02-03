66c3ab51d6756274c6d8973492e602f3547948b1
maryferrer <85918290+maryferrer@users.noreply.github.com>
2022-02-02 18:28:34 -0800

    useState for meme image, create getMemeImage function to display random meme on page

--- src/Meme.js
+++ src/Meme.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import memeObjects from './memesData';
 
 function Meme() {
+    const [memeImage, setMemeImage] = React.useState("")
+
+    function getMemeImage() {
+       const memesArray = memeObjects.data.memes
+       const randomNumber = Math.floor(Math.random() * memesArray.length)
+       setMemeImage(memesArray[randomNumber].url)
+    }
+
     return (
-        <div className="form">
-            <input className="form--input" type="text" placeholder="top text"></input>
-            <input className="form--input" type="text" placeholder="bottom text"></input>
-            <button className="form--button" onClick>Get a new meme image</button>
-        </div>
+        <main>
+            <div className="form">
+                <input className="form--input" type="text" placeholder="top text"></input>
+                <input className="form--input" type="text" placeholder="bottom text"></input>
+                <button className="form--button" onClick={getMemeImage}>Get a new meme image</button>
+            </div>
+            <img src={memeImage} alt="meme" className="meme--image" />
+        </main>
     )
 }
 
 export default Meme;
-
