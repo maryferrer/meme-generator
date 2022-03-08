7c2885a3314d6ab9e1666bda17812a6cec2f5a5f
maryferrer <85918290+maryferrer@users.noreply.github.com>
2022-03-08 10:29:49 -0800

    add text input fields and new image button

--- src/Meme.js
+++ src/Meme.js
@@ -7,20 +7,39 @@ function Meme() {
     function getMemeImage() {
        const memesArray = memeObjects.data.memes
        const randomNumber = Math.floor(Math.random() * memesArray.length)
-     function handleChange(event) {
-        const {name, value} = event.target
-        setMeme(prevMeme => ({
-            ...prevMeme,
-            [name]: value
-        }))
+       setMemeImage(memesArray[randomNumber].url)
     }
 
     return (
         <main>
             <div className="form">
-                <input className="form--input" type="text" placeholder="top text"></input>
-                <input className="form--input" type="text" placeholder="bottom text"></input>
-                <button className="form--button" onClick={getMemeImage}>Get a new meme image</button>
+                <input 
+                    name="topText"
+                    onChange={handleChange}
+                    value={meme.topText}
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                />
+                <input
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                />
+                <button 
+                    className="form--button"
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image
+                </button>
+            </div>
+            <div className="meme">
+                <img src={meme.randomImage} className="meme--image" alt="meme"/>
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
             </div>
             <img src={memeImage} alt="meme" className="meme--image" />
         </main>
