dbccb8108e4beed139820696004828fb5ff36300
maryferrer <85918290+maryferrer@users.noreply.github.com>
2022-03-08 11:01:23 -0800

    remove image

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,6 @@ import React from 'react'
 function Header() {
     return (
     <header className="header">
-            <img src="/Troll Face.png" alt="Troll Face meme" className="troll" />
             <h2 className="header--title">Meme Generator</h2>
         <h4 className="header--subtitle">React Course - Project 3</h4>
     </header>
