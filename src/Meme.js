a345cde59367c69b85ad303b37342a2e439c7e84
maryferrer <85918290+maryferrer@users.noreply.github.com>
2022-03-08 10:30:33 -0800

    meme initial state, setMeme state, fetch request to memes api

--- src/Meme.js
+++ src/Meme.js
@@ -1,15 +1,43 @@
 import React from 'react';
-import memeObjects from './memesData';
 
 function Meme() {
-    const [memeImage, setMemeImage] = React.useState("")
 
+const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
+    })
+    
+    const [allMemes, setAllMemes] = React.useState([])
+
+    React.useEffect(() => {
+        async function getMemes() {
+        const res = await fetch("https://api.imgflip.com/get_memes")
+        const data = await res.json()
+        setAllMemes(data.data.memes)
+        }
+        getMemes()
+    }, [])
+    
     function getMemeImage() {
-       const memesArray = memeObjects.data.memes
-       const randomNumber = Math.floor(Math.random() * memesArray.length)
-       setMemeImage(memesArray[randomNumber].url)
+        const memesArray = allMemes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+        
     }
 
+     function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+    
     return (
         <main>
             <div className="form">
@@ -41,9 +69,9 @@ function Meme() {
                 <h2 className="meme--text top">{meme.topText}</h2>
                 <h2 className="meme--text bottom">{meme.bottomText}</h2>
             </div>
-            <img src={memeImage} alt="meme" className="meme--image" />
         </main>
     )
 }
 
 export default Meme;
+
